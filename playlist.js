7ed7fc9e0eddaa59ac32e474a480cc6bdd5cade8
DigiBoy <nitinsingh10207@gmail.com>
2026-08-15 15:58:24 +0530

    adding playlists

--- playlist.js
+++ playlist.js
@@ -11,11 +11,16 @@ const ADMIN_PASSWORD = "160610#";
 //    Give each a friendly "name" — that's what shows as a button in the admin panel.
 //    You can leave unused ones as empty string "" — they just won't show a button.
 const PLAYLISTS = [
-  { key: "main",  name: "Main Radio",  playlistId: "PLxzEzcNbKPvqALUUN3Bplq6E3Enhp5jYz" },
-  { key: "list2", name: "Playlist 2",  playlistId: "PLGuZiHGXiIdg" },
-  { key: "list3", name: "Playlist 3",  playlistId: "PLxzEzcNbKPvq2kZynKOOzH0nTYVziw8Db" },
-  { key: "list4", name: "Playlist 4",  playlistId: "PLxzEzcNbKPvrz9s7dPxdrVKjj4cv0LWbm" },
-  { key: "list5", name: "Playlist 5",  playlistId: "PLXl73k5eV9kE" },
+  { key: "main",  name: "Mood-romantic",  playlistId: "PLxzEzcNbKPvrz9s7dPxdrVKjj4cv0LWbm" },
+  { key: "main6",  name: "bhojpuri 6",  playlistId: "PLK_nHcQFSPbE" },
+  { key: "list2", name: " bhoothnath",  playlistId: "PLGuZiHGXiIdg" },
+  { key: "list3", name: "mood-haryanvi",  playlistId: "PLXl73k5eV9kE" },
+  { key: "list5", name: "mood 5 ",  playlistId: "PLxzEzcNbKPvq2kZynKOOzH0nTYVziw8Db" },
+  { key: "list6", name: "bhakti",  playlistId: "PLSOiXgZ_T7o8" },
+  { key: "list7", name: "bus 8",  playlistId: "PLxzEzcNbKPvqQDSwKF9ou632MMucDOyv2" },
+  { key: "list8", name: "Playlist 9",  playlistId: "PLTqEOF_NFMhw" },
+  { key: "list9", name: "free",  playlistId: "PLJjZJoUdCWHU" },
+  { key: "list4", name: "bairan",  playlistId: "PLTgmeJQIet5A" },
 ];
 
 module.exports = { YOUTUBE_API_KEY, ADMIN_PASSWORD, PLAYLISTS };
